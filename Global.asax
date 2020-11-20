62c3f13458fc732d88161337c8ad52e0a8d8b6c8
Marcelo Echeverria <marceloecheverria23@gmail.com>
2020-11-20 00:36:20 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="puceAsk_dev1.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(puceAsk_dev1.Startup))]
+namespace puceAsk_dev1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
