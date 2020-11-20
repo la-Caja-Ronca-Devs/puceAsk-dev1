da6825ce2115d4234e57472a60609f1b68fcad2b
Valeria De la Torre <vale_dlt98@hotmail.com>
2020-11-20 10:53:05 +0000

    nabvar

---
+++ Controllers/PreguntasController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using puceAsk_dev1.Models;
+
+namespace puceAsk_dev1.Controllers
+{
+    public class PreguntasController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Preguntas
+        public ActionResult Index()
+        {
+            return View(db.Pregunta.ToList());
+        }
+
+        public ActionResult PreguntasManager(int? id, int? respuestaID)
+        {
+            var viewModel = new PreguntasManager();
+            viewModel.preguntas = db.Pregunta
+                .Include(i => i.Respuestas.Select(c => c.Cuenta));
+
+            return View(viewModel);
+        }
+
+        public ActionResult Inicio()
+        {
+            var viewModel = new PreguntasManager();
+            viewModel.preguntas = db.Pregunta
+                .Include(i => i.Respuestas.Select(c => c.Cuenta));
+
+            
+            return View(viewModel);
+        }
+
+        // GET: Preguntas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pregunta pregunta = db.Pregunta.Find(id);
+            if (pregunta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pregunta);
+        }
+
+        // GET: Preguntas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Preguntas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PreguntaId,RowVersion,TituloPregunta,DescPregunta,Fechapregunta")] Pregunta pregunta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pregunta.Add(pregunta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pregunta);
+        }
+
+        // GET: Preguntas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pregunta pregunta = db.Pregunta.Find(id);
+            if (pregunta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pregunta);
+        }
+
+        // POST: Preguntas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PreguntaId,RowVersion,TituloPregunta,DescPregunta,Fechapregunta")] Pregunta pregunta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pregunta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pregunta);
+        }
+
+        // GET: Preguntas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pregunta pregunta = db.Pregunta.Find(id);
+            if (pregunta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pregunta);
+        }
+
+        // POST: Preguntas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pregunta pregunta = db.Pregunta.Find(id);
+            db.Pregunta.Remove(pregunta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/PreguntasManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace puceAsk_dev1.Models
+{
+    public class PreguntasManager
+    {
+        public IEnumerable<Pregunta> preguntas { get; set; }
+        public IEnumerable<Respuesta> respuestas { get; set; }
+    }
+}
